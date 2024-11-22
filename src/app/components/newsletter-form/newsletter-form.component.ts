99b830678e4fe9b0fa41802b941d3dee7f8ad046
chrisbmineiro <chrisbmineiro@gmail.com>
2024-11-22 17:42:10 -0300

    feat: adding newsletter-form component

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
+import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'newsletter-form',
+  standalone: true,
+  imports: [
+    BtnPrimaryComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.css'
+})
+export class NewsletterFormComponent {
+  newsletterForm: FormGroup;
+
+  constructor(){
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+}
